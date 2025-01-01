1fb00a91a06997754cd67abf21498efdd5b87eb6
Timothy Johansen <tim.johansen1@gmail.com>
2025-01-01 14:07:25 -0800

    Some reformatting and variable renaming.

--- AE_Mask_To_PP.py
+++ AE_Mask_To_PP.py
@@ -9,26 +9,32 @@ import xml.etree.ElementTree as ET
 import struct
 import base64
 import json
-# import typing
 
-ticks_per_second = 254016000000
+time_units_per_second = 254016000000
 
 
 class MaskPoint:
-    def __init__(self, vertex: tuple[float, float], out_tan: tuple[float, float], in_tan: tuple[float, float]):
+    def __init__(self,
+                 vertex: tuple[float, float],
+                 out_tan: tuple[float, float],
+                 in_tan: tuple[float, float]):
         self.vertex = vertex
         self.out_tan = out_tan
         self.in_tan = in_tan
 
 
 class MaskShape:
-    def __init__(self, mask_points: list[MaskPoint], is_closed: bool):
+    def __init__(self,
+                 mask_points: list[MaskPoint],
+                 is_closed: bool):
         self.points = mask_points
         self.is_closed = is_closed
 
 
 class MaskKeyframe:
-    def __init__(self, time: float, shape: MaskShape):
+    def __init__(self,
+                 time: float,
+                 shape: MaskShape):
         self.time = time
         self.shape = shape
 
@@ -36,11 +42,10 @@ class MaskKeyframe:
 def paste_from_premiere():
     if platform.system() == "Darwin":
         pb = NSPasteboard.generalPasteboard()
-
         for t in pb.pasteboardItems()[0].types():
-            data = pb.pasteboardItems()[0].stringForType_(t)
+            raw_data = pb.pasteboardItems()[0].stringForType_(t)
             try:
-                parsed_data = ET.fromstring(data)
+                parsed_data = ET.fromstring(raw_data)
                 if parsed_data.tag == "PremiereData":
                     premiere_data = pb.pasteboardItems()[0].stringForType_(t)
                     return t, premiere_data
@@ -50,16 +55,15 @@ def paste_from_premiere():
                 continue
         print("Invalid Premiere clipboard data")
         exit()
-    # Windows
     # elif platform.system() == "Windows":
     #     w32cb.OpenClipboard()
-    #     premiere_data = w32cb.GetClipboardData(49879)
     #     owner = w32cb.GetClipboardOwner()
+    #     premiere_data = w32cb.GetClipboardData(49879)
     #     w32cb.CloseClipboard()
     #     return owner, premiere_data
 
 
-def get_start_tick(premiere_clipboard: str):
+def get_clip_start_time(premiere_clipboard: str):
     parsed_data = ET.fromstring(premiere_clipboard)
     iterator = parsed_data.iterfind("VideoClip")
     while True:
@@ -84,12 +88,12 @@ def paste_from_ae():
     #     w32cb.CloseClipboard()
 
     try:
-        pasted_json_data = json.loads(clipboard_string)
+        json_object = json.loads(clipboard_string)
     except json.decoder.JSONDecodeError:
         print("Clipboard does not appear to contain AE mask data.")
         return
     processed_keys = []
-    for ae_key in pasted_json_data:
+    for ae_key in json_object:
         points = []
         for point in ae_key['points']:
             vertex = point['vertex'].split(sep=',')
@@ -104,19 +108,20 @@ def paste_from_ae():
 
 
 def mask_shape_to_b64(shape: MaskShape, is_first_key=False):
-    # The first few bytes are sometimes b'\x6b\x63\x69\x6e\x02\x00\x00\x00', but the below seems to work consistently.
+    # The first few bytes are sometimes b'\x6b\x63\x69\x6e\x02\x00\x00\x00', and I'm not sure why,
+    # but the sequence below seems to work consistently.
     raw_data = b'\x32\x63\x69\x6e\x02\x00\x00\x00'
     if shape.is_closed:
         raw_data += b'\x00'
     else:
         raw_data += b'\x02'
-    raw_data += struct.pack('>i', len(shape.points))    # '>' specifies big-endian
+    raw_data += struct.pack('>i', len(shape.points))    # '>i' specifies big-endian integer.
     raw_data += b'\x00\x00\x00'
     for point in shape.points:
         if point.in_tan == point.vertex and point.out_tan == point.vertex:
             raw_data += b'\x00\x00\x00\x00'
         else:
-            raw_data += b'\x01\x00\x00\x00'
+            raw_data += b'\x01\x00\x00\x00'             # Specifies that the point is part of a curve.
 
         raw_data += struct.pack('f', point.vertex[0])
         raw_data += struct.pack('f', point.vertex[1])
@@ -129,16 +134,18 @@ def mask_shape_to_b64(shape: MaskShape, is_first_key=False):
     return base64.b64encode(raw_data)
 
 
-def create_premiere_keyframes(keyframes: list[MaskKeyframe], start_tick) -> str:
-    string = ""
+def create_premiere_keyframes(keyframes: list[MaskKeyframe],
+                              start_time) -> str:
+    keyframe_string = ""
     for i in range(len(keyframes)):
         key = keyframes[i]
-        string += str(int(key.time * ticks_per_second + start_tick)) + ','
-        string += mask_shape_to_b64(key.shape, i == 0).decode() + ';'
-    return string
+        keyframe_string += str(int(key.time * time_units_per_second + start_time)) + ','
+        keyframe_string += mask_shape_to_b64(key.shape, i == 0).decode() + ';'
+    return keyframe_string
 
 
-def modify_premiere_clipboard_data(premiere_clipboard: str, encoded_keyframes: str):
+def modify_premiere_clipboard_data(premiere_clipboard: str,
+                                   encoded_keyframes: str):
     root = ET.fromstring(premiere_clipboard)
     iterator = root.iterfind("ArbVideoComponentParam")
     while True:
@@ -155,20 +162,22 @@ def modify_premiere_clipboard_data(premiere_clipboard: str, encoded_keyframes: s
     return str(ET.tostring(root).decode())
 
 
-def replace_clipboard(keyframe_data: str, premiere_clipboard_data: str, premiere_UTI: str):
+def replace_clipboard(keyframe_data: str,
+                      premiere_clipboard_data: str,
+                      premiere_ID: str):
     final_string = modify_premiere_clipboard_data(premiere_clipboard_data, keyframe_data)
     if platform.system() == "Darwin":
         allocated_string = NSString.alloc().initWithString_(final_string)
         pasteboard = NSPasteboard.generalPasteboard()
         pasteboard.clearContents()
-        pasteboard.declareTypes_owner_([premiere_UTI], None)
-        if pasteboard.pasteboardItems()[0].setString_forType_(allocated_string, premiere_UTI):
+        pasteboard.declareTypes_owner_([premiere_ID], None)
+        if pasteboard.pasteboardItems()[0].setString_forType_(allocated_string, premiere_ID):
             return
         else:
             print("Couldn't replace clipboard data.")
-    # Windows
+            exit()
     # elif platform.system() == "Windows":
-    #     w32cb.OpenClipboard(premiere_UTI)
+    #     w32cb.OpenClipboard(premiere_ID)
     #     w32cb.EmptyClipboard()
     #     w32cb.SetClipboardData(49876, final_string)
     #     w32cb.SetClipboardData(49879, final_string)
@@ -181,7 +190,7 @@ premiere_UTI, premiere_clipboard_data = paste_from_premiere()
 input("In After Effects, select the mask you want to copy, run the JSX script. and copy the results. \n\
     Press enter in this window when ready.")
 keys = paste_from_ae()
-start_tick = get_start_tick(premiere_clipboard_data)
-encoded_keys = create_premiere_keyframes(keys, start_tick)
+clip_start_time = get_clip_start_time(premiere_clipboard_data)
+encoded_keys = create_premiere_keyframes(keys, clip_start_time)
 replace_clipboard(encoded_keys, premiere_clipboard_data, premiere_UTI)
 print("Mask is ready to paste into Premiere.")
