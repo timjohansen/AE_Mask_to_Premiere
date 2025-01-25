82c9a6413ffe2e1e48a942c0577da07c60a1fc1a
Timothy Johansen <tim.johansen1@gmail.com>
2025-01-24 21:57:11 -0800

    UI implemented. Premiere clip start time is now accounted for.

--- AE_Mask_To_PP.py
+++ AE_Mask_To_PP.py
@@ -9,6 +9,8 @@ import xml.etree.ElementTree as ET
 import struct
 import base64
 import json
+import tkinter
+from tkinter import ttk, N,S,E,W
 
 time_units_per_second = 254016000000
 
@@ -42,40 +44,191 @@ class MaskKeyframe:
 def paste_from_premiere():
     if platform.system() == "Darwin":
         pb = NSPasteboard.generalPasteboard()
+        parsed_data = None
+        pb_type = None
+        premiere_data_string = None
+        clip_name = ""
+
         for t in pb.pasteboardItems()[0].types():
             raw_data = pb.pasteboardItems()[0].stringForType_(t)
             try:
                 parsed_data = ET.fromstring(raw_data)
                 if parsed_data.tag == "PremiereData":
-                    premiere_data = pb.pasteboardItems()[0].stringForType_(t)
-                    return t, premiere_data
+                    premiere_data_string = pb.pasteboardItems()[0].stringForType_(t)
+                    pb_type = t
+                    break
             except ET.ParseError:
                 continue
             except TypeError:
                 continue
-        print("Invalid Premiere clipboard data")
-        exit()
+        if premiere_data_string is None:
+            print("Premiere data not found on clipboard.")
+            return None
+
+        iterator = parsed_data.iterfind("ArbVideoComponentParam")
+
+        while True:
+            try:
+                node = next(iterator)
+                name_node = node.find("Name")
+                if name_node is None or name_node.text != "Mask Path":
+                    continue
+                keyframes_node = node.find("Keyframes")
+                if keyframes_node is not None:
+                    break
+            except StopIteration:
+                print("Keyframes not found in Premiere clipboard")
+                exit()
+
+        try:
+            object_id = int(node.attrib["ObjectID"])
+        except:
+            print("Could not parse ArbVideoComponentParam object id")
+            exit()
+
+        iterator = parsed_data.iterfind("VideoFilterComponent")
+        found = False
+        while True:
+            try:
+                node = next(iterator)
+                vfc_node = node
+                node = node.find("Component")
+                node = node.find("Params")
+                param_iter = node.iterfind("Param")
+                while True:
+                    try:
+                        param_node = next(param_iter)
+                        if int(param_node.attrib["ObjectRef"]) == object_id:
+                            object_id = int(vfc_node.attrib["ObjectID"])
+                            found = True
+                            break
+                    except StopIteration:
+                        break
+            except StopIteration:
+                break
+        if not found:
+            print("Error")
+            exit()
+
+        iterator = parsed_data.iterfind("VideoFilterComponent")
+        found = False
+        while True:
+            try:
+                node = next(iterator)
+                vfc_node = node
+                node = node.find("SubComponents")
+                if node is None:
+                    continue
+                sub_component_iter = node.iterfind("SubComponent")
+                while True:
+                    try:
+                        subcom_node = next(sub_component_iter)
+                        if int(subcom_node.attrib["ObjectRef"]) == object_id:
+                            object_id = int(vfc_node.attrib["ObjectID"])
+                            found = True
+                            break
+                    except StopIteration:
+                        break
+            except StopIteration:
+                break
+        if not found:
+            print("Error")
+            exit()
+
+        iterator = parsed_data.iterfind("VideoComponentChain")
+        found = False
+        while True:
+            try:
+                node = next(iterator)
+                vcc_node = node
+                node = node.find("ComponentChain")
+                node = node.find("Components")
+                component_iter = node.iterfind("Component")
+                while True:
+                    try:
+                        com_node = next(component_iter)
+                        if int(com_node.attrib["ObjectRef"]) == object_id:
+                            object_id = int(vcc_node.attrib["ObjectID"])
+                            found = True
+                            break
+                    except StopIteration:
+                        break
+            except StopIteration:
+                break
+        if not found:
+            print("Error")
+            exit()
+
+        iterator = parsed_data.iterfind("VideoClipTrackItem")
+        found = False
+        while True:
+            try:
+                node = next(iterator)
+                try:
+                    ct_node = node.find("ClipTrackItem")
+                    node = ct_node.find("ComponentOwner")
+                    node = node.find("Components")
+                    if object_id == int(node.attrib["ObjectRef"]):
+                        found = True
+                        node = ct_node.find("SubClip")
+                        object_id = int(node.attrib["ObjectRef"])
+                except:
+                    continue
+
+            except StopIteration:
+                break
+        if not found:
+            print("Error")
+            exit()
+
+        iterator = parsed_data.iterfind("SubClip")
+        found = False
+        while True:
+            try:
+                node = next(iterator)
+                clip_node = node.find("Clip")
+                object_id = int(clip_node.attrib["ObjectRef"])
+                node = node.find("Name")
+                clip_name = node.text
+                found = True
+                break
+            except StopIteration:
+                break
+        if not found:
+            print("Error")
+            exit()
+
+        iterator = parsed_data.iterfind("VideoClip")
+        found = False
+        while True:
+            try:
+                node = next(iterator)
+                if int(node.attrib["ObjectID"]) != object_id:
+                    continue
+                node = node.find("Clip")
+                in_point_node = node.find("InPoint")
+                if in_point_node is not None:
+                    try:
+                        in_point = int(in_point_node.text)
+                        found = True
+                        break
+                    except:
+                        print("Error")
+                        exit()
+            except StopIteration:
+                break
+        if not found:
+            print("Error")
+            exit()
+
+        return premiere_data_string, pb_type, in_point, clip_name
+
     # elif platform.system() == "Windows":
     #     w32cb.OpenClipboard()
     #     owner = w32cb.GetClipboardOwner()
     #     premiere_data = w32cb.GetClipboardData(49879)
     #     w32cb.CloseClipboard()
     #     return owner, premiere_data
-
-
-def get_clip_start_time(premiere_clipboard: str):
-    parsed_data = ET.fromstring(premiere_clipboard)
-    iterator = parsed_data.iterfind("VideoClip")
-    while True:
-        try:
-            node = next(iterator)
-            clip_node = node.find("Clip")
-            in_point_node = clip_node.find("InPoint")
-            if in_point_node is not None:
-                return int(in_point_node.text)
-        except StopIteration:
-            print("Video Clip data not found on clipboard.")
-            exit()
 
 
 def paste_from_ae():
@@ -184,13 +337,89 @@ def replace_clipboard(keyframe_data: str,
     #     w32cb.CloseClipboard()
 
 
-input("In Premiere, add a keyframed mask to the clip you want to have the mask, and copy it to the clipboard.\n\
-    Press enter in this window when ready.")
-premiere_UTI, premiere_clipboard_data = paste_from_premiere()
-input("In After Effects, select the mask you want to copy, run the JSX script. and copy the results. \n\
-    Press enter in this window when ready.")
-keys = paste_from_ae()
-clip_start_time = get_clip_start_time(premiere_clipboard_data)
-encoded_keys = create_premiere_keyframes(keys, clip_start_time)
-replace_clipboard(encoded_keys, premiere_clipboard_data, premiere_UTI)
-print("Mask is ready to paste into Premiere.")
+def premiere_button_clicked():
+    global premiere_status
+    global premiere_ID
+    global premiere_clipboard_data
+    global premiere_in_point
+    data = paste_from_premiere()
+    if data is None:
+        return
+    premiere_clipboard_data, premiere_ID, premiere_in_point, clip_name = data
+    premiere_status.set("Video Clip:\n" + clip_name)
+    check_if_ready()
+
+
+def check_if_ready():
+    if ae_clipboard_data is not None and premiere_clipboard_data is not None:
+        process_button.config(state=tkinter.NORMAL, text="Click to Copy")
+    else:
+        process_button.config(state=tkinter.DISABLED, text="Not Ready")
+
+
+def ae_button_clicked():
+    global ae_clipboard_data
+    global ae_status
+    data = paste_from_ae()
+    if data is not None:
+        ae_clipboard_data = data
+        ae_status.set("Keyframes: " + str(len(ae_clipboard_data)))
+        check_if_ready()
+
+
+def convert_button_clicked():
+    if ae_clipboard_data is None or premiere_clipboard_data is None:
+        print("Error, the button should not have been clickable")
+        return None
+    encoded_keys = create_premiere_keyframes(ae_clipboard_data, premiere_in_point)
+    replace_clipboard(encoded_keys, premiere_clipboard_data, premiere_ID)
+    print("Success")
+
+
+# Globals
+premiere_clipboard_data = None
+ae_clipboard_data = None
+premiere_ID = None
+premiere_in_point = None
+
+# UI
+window = tkinter.Tk()
+window.title("AE Mask to Premiere")
+main_frame = ttk.Frame(window, padding=10)
+main_frame.grid(column=0, row=0, sticky=(N,W,E,S))
+
+paste_frame = ttk.Frame(main_frame, padding=10)
+paste_frame.grid(column=0, row=0, sticky=(N,W,E,S))
+
+premiere_paste_frame = ttk.Frame(paste_frame, padding=5, borderwidth=1, relief="solid")
+premiere_paste_frame.grid(column=0, row=0, sticky=(N,S))
+premiere_paste_button = ttk.Button(premiere_paste_frame, text="Paste from Premiere", command=premiere_button_clicked)
+premiere_paste_button.grid(column=0, row=0)
+
+ae_paste_frame = ttk.Frame(paste_frame, padding=5, borderwidth=1, relief="solid")
+ae_paste_frame.grid(column=2, row=0, sticky=(N,S))
+ae_paste_button = ttk.Button(ae_paste_frame, text="Paste from After Effects", command=ae_button_clicked)
+ae_paste_button.grid(column=0, row=0)
+
+premiere_status = tkinter.StringVar()
+premiere_status.set("Video Clip: (none)")
+ae_status = tkinter.StringVar()
+ae_status.set("Keyframes: (none)")
+
+premiere_status_label = ttk.Label(premiere_paste_frame, padding=5, textvariable=premiere_status, wraplength=100)
+premiere_status_label.config(state="disabled")
+premiere_status_label.grid(column=0, row=1)
+
+ae_status_label = ttk.Label(ae_paste_frame, padding=5, textvariable=ae_status, wraplength=100)
+ae_status_label.config(state="disabled")
+ae_status_label.grid(column=0, row=1)
+
+process_frame = ttk.Frame(main_frame, padding=10, borderwidth=1, relief="solid")
+process_frame.grid(column=0, row=1, sticky=(N,W,E,S))
+
+process_button = ttk.Button(main_frame, text="Not Ready", command=convert_button_clicked)
+process_button.grid(column=0, row=3, sticky=(N,W,E,S))
+process_button.config(state=tkinter.DISABLED)
+
+
+window.mainloop()
